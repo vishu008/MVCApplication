30bd5afdb3d56c65f7da508c38eecd074f741366
Vishal Parbhakar <vishalparbhakar008@gmail.com>
2017-05-25 23:03:22 +0530

    Initial check-in

---
+++ RouteConfig.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+
+public class RouteConfig 
+{
+    public static void RegisterRoutes(RouteCollection routes)
+    {
+    
+    }
+}
